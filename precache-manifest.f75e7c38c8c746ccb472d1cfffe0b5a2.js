5835557f4ce9eeed0b2c605ef107f8b5cc8343b0
ArturAquino <uchihaartur@yahoo.com.br>
2019-05-28 12:01:24 -0300

    Updates

---
+++ precache-manifest.f75e7c38c8c746ccb472d1cfffe0b5a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a6d903e0c19e49e8686249d7cb4d9f7",
+    "url": "/nps-test/index.html"
+  },
+  {
+    "revision": "18757ac3525074778c35",
+    "url": "/nps-test/static/css/main.cdde3d1c.chunk.css"
+  },
+  {
+    "revision": "4b5bc1076335992e8cda",
+    "url": "/nps-test/static/js/2.ed17e046.chunk.js"
+  },
+  {
+    "revision": "18757ac3525074778c35",
+    "url": "/nps-test/static/js/main.bcff354c.chunk.js"
+  },
+  {
+    "revision": "008f7656c20048e227ec",
+    "url": "/nps-test/static/js/runtime~main.b79788e2.js"
+  },
+  {
+    "revision": "c44d98a67ac5cb599d8a5983cb7636c7",
+    "url": "/nps-test/static/media/clap.c44d98a6.gif"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/nps-test/static/media/logo.ee7cd8ed.svg"
+  }
+]);
